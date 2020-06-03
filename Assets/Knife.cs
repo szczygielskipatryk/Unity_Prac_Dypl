66d0b67985780a089ade9b276c43b21efedf8405
Patryk(or Patrick idc) <32666895+szczygielskipatryk@users.noreply.github.com>
2020-06-03 16:57:31 +0200

    init commit

---
+++ Assets/Knife.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+public class Knife : MonoBehaviour
+{
+
+    
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit.collider != null)
+            {
+                if (hit.collider.tag == "Link")
+                {
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+        }
+    }
+}
